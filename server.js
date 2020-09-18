f4439ec0a4b754eafacf596625e0475fab88c0fc
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-09-18 13:42:02 -0500

    Convolucion

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works! P5.js\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+var path = require('path');
+var fs = require('fs');
+var server = http.createServer(handleRequest);
+server.listen(3000);
+//server.listen();
+console.log('Server started on port 3000');
+function handleRequest(req, res) {
+  var pathname = req.url;
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+  var ext = path.extname(pathname);
+  var typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+  var contentType = typeExt[ext] || 'text/plain';
+  fs.readFile(__dirname + pathname,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
