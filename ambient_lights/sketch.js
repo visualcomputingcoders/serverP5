33b89f3572e3c9a9d5ead9cb673b677d39066ee5
Kevin Andrey Herrera Silva <kahs_kevin@hotmail.com>
2020-11-22 14:58:28 -0500

    Ambient Lights

---
+++ ambient_lights/sketch.js
@@ -0,0 +1,41 @@
+let newFont; 
+let pointLightEnable = false; 
+  
+function preload() { 
+  newFont = loadFont('fonts/Montserrat.otf'); 
+} 
+  
+function setup() { 
+  createCanvas(600, 300, WEBGL); 
+  textFont(newFont, 18); 
+  
+  graySlider = createSlider(0, 128, 64, 1); 
+  graySlider.position(20, 50); 
+  
+  pointLightCheck = createCheckbox( 
+        "Enable Point Light", false); 
+  
+  pointLightCheck.position(20, 80); 
+  
+  // Toggle point light 
+  pointLightCheck.changed(() => { 
+    pointLightEnable = !pointLightEnable; 
+  }); 
+} 
+  
+function draw() { 
+  background("green"); 
+  text("Move the slider to change the ambient"
+      + " light's gray value.", -285, -125); 
+  noStroke(); 
+  shininess(15); 
+  if (pointLightEnable) { 
+    pointLight(255, 0, 0, -width / 2, 
+                    -height / 2, 250); 
+  } 
+  
+  grayValue = graySlider.value(); 
+  ambientLight(grayValue); 
+  specularMaterial(250); 
+  sphere(100); 
+} 
